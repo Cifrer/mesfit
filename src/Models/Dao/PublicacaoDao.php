fdf5b11ed67e77a7faaa9bb5ebcca91d95b8fd6b
Cifrer <69824613+Cifrer@users.noreply.github.com>
2020-11-05 01:33:10 -0300

    Update PublicacaoDao.php

--- src/Models/Dao/PublicacaoDao.php
+++ src/Models/Dao/PublicacaoDao.php
@@ -10,7 +10,7 @@ class PublicacaoDao
     public function create(Publicacao $p)
     {
 
-        $sql = 'INSERT INTO Publicacao (tituloPub, rankPub, descPub, dtHrPub, codStatusPub) VALUES (?,?,?,?,?)';
+        $sql = 'INSERT INTO publicacao (tituloPub, rankPub, descPub, dtHrPub, codStatusPub) VALUES (?,?,?,?,?)';
 
         $cadastrar = Conexao::getConn()->prepare($sql);
         $cadastrar->bindValue(1, $p->getTituloPub());
@@ -24,6 +24,20 @@ class PublicacaoDao
 
     public function read()
     {
+        try{
+            $sql = 'SELECT * FROM publicacao';
+            $listar = Conexao::getConn()->prepare($sql);
+            $listar->execute();
+
+            if ($listar->rowCount() > 0) {
+                $result = $listar->fetchAll(\PDO::FETCH_ASSOC);
+                return $result;
+            } else {
+                return [];
+            }
+        } catch (\PDOException $exception) {
+            throw $exception;{
+        }
     }
 
     public function update(Publicacao $p)

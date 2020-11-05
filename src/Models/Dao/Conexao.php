fab893e5aee0f69a35a54dffbddc719babf7bc97
Cifrer <69824613+Cifrer@users.noreply.github.com>
2020-11-05 00:29:50 -0300

    Update Conexao.php

--- src/Models/Dao/Conexao.php
+++ src/Models/Dao/Conexao.php
@@ -9,7 +9,7 @@ class Conexao
   public static function getConn()
   {
     if (!isset(self::$instance)) :
-      self::$instance = new \PDO('mysql:host=localhost;dbname=bdmesfit;charset=utf8mb4', 'root', '');
+      self::$instance = new \PDO('mysql:host=localhost;dbname=bdmesfit;charset=utf8mb4', 'root', 'root');
     endif;
     return self::$instance;
   }

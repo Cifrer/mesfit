84e19b605c125dac5bd6fe61469ff8410060efdd
Cifrer <69824613+Cifrer@users.noreply.github.com>
2020-11-06 09:48:00 -0300

    chat

--- templates/client/mensagens.php
+++ templates/client/mensagens.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet"  href="<?= url("templates/global/default.css") ?>" />
+  <link rel="stylesheet"  href="<?= url("templates/global/css/default.css") ?>" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <script src="https://kit.fontawesome.com/223af74c50.js" crossorigin="anonymous"></script>

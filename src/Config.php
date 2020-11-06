35422a11cb761e56a0756ec8c77eb53f2fdeedd9
Cifrer <69824613+Cifrer@users.noreply.github.com>
2020-11-06 14:20:18 -0300

    config root host

--- src/Config.php
+++ src/Config.php
@@ -3,7 +3,7 @@ if (empty(session_start())) {
   session_start();
 }
 
-#define("ROOT", isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'] . "/sites/mesfit");
+#define("ROOT", isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']);
 define("ROOT", "http://localhost/sites/mesfit");
 define("URL_VIEW_ADMIN", "../../templates/admin");
 define("URL_VIEW_WEB", "../../templates/web");
